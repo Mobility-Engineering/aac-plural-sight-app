04b53381af382e1b8c4af17402472480e053dd90
Daniel Vidales <daniel.vidales@dexcom.com>
2022-06-21 06:03:56 -0700

    Migrates needed content from NoteListActivity to Home Fragment for identical behavior of its RecyclerView but also  Updates onResume callback to reflect changes of notes

--- app/src/main/java/com/dexcom/sdk/aac_fullcontentapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/dexcom/sdk/aac_fullcontentapp/ui/home/HomeFragment.kt
@@ -26,6 +26,8 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private var noteRecyclerAdapter: NoteRecyclerAdapter? = null
+    private lateinit var adapterNotes: ArrayAdapter<NoteInfo>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,6 +38,21 @@ class HomeFragment : Fragment() {
         val root: View = binding.root
         initializeDisplayContent()
         return root
+    }
+
+    override fun onResume() {
+        noteRecyclerAdapter?.notifyDataSetChanged()
+        super.onResume()
+    }
+
+    private fun initializeDisplayContent() {
+        val context = activity as Context
+        val recyclerNotes = binding.listNotes
+        val notesLayoutManager = LinearLayoutManager(context)
+        recyclerNotes.layoutManager = notesLayoutManager
+        val notes = DataManager.instance?.notes
+        noteRecyclerAdapter = notes?.let{NoteRecyclerAdapter(context, it) }
+        recyclerNotes.adapter = noteRecyclerAdapter
     }
 
     override fun onDestroyView() {
