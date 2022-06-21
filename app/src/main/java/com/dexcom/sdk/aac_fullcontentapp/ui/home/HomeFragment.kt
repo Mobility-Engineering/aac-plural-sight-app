39ed49d23c505bafae4667a7d155b11610acd035
Daniel Vidales <daniel.vidales@dexcom.com>
2022-06-21 06:03:11 -0700

    Migrates needed content from NoteListActivity to Home Fragment for identical behavior of its RecyclerView but also  Updates onResume callback to reflect changes of notes

--- app/src/main/java/com/dexcom/sdk/aac_fullcontentapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/dexcom/sdk/aac_fullcontentapp/ui/home/HomeFragment.kt
@@ -1,12 +1,22 @@
 package com.dexcom.sdk.aac_fullcontentapp.ui.home
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dexcom.sdk.aac_fullcontentapp.DataManager
+import com.dexcom.sdk.aac_fullcontentapp.NoteActivity
+import com.dexcom.sdk.aac_fullcontentapp.NoteInfo
+import com.dexcom.sdk.aac_fullcontentapp.NoteRecyclerAdapter
+import com.dexcom.sdk.aac_fullcontentapp.databinding.ActivityNoteList2Binding
 import com.dexcom.sdk.aac_fullcontentapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -22,16 +32,9 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        initializeDisplayContent()
         return root
     }
 
