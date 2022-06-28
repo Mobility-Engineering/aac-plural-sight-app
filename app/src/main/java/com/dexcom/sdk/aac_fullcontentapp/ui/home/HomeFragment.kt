8196c53498851ed29b5b04d6b3bce0baa7448ca9
Daniel Vidales <daniel.vidales@dexcom.com>
2022-06-28 04:08:33 -0700

    SQLiteOpenHelper  is used to create Database and load RecyclerAdapter Data from it (synchronous operation)

--- app/src/main/java/com/dexcom/sdk/aac_fullcontentapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/dexcom/sdk/aac_fullcontentapp/ui/home/HomeFragment.kt
@@ -1,8 +1,10 @@
 package com.dexcom.sdk.aac_fullcontentapp.ui.home
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,6 +17,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.dexcom.sdk.aac_fullcontentapp.*
+import com.dexcom.sdk.aac_fullcontentapp.database.NoteKeeperOpenHelper
 import com.dexcom.sdk.aac_fullcontentapp.databinding.ActivityNoteList2Binding
 import com.dexcom.sdk.aac_fullcontentapp.databinding.FragmentHomeBinding
 
@@ -22,11 +25,12 @@ class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private var noteListSize = DataManager.instance?.notes?.size!!
+    private var noteListSize = 1
     private val binding get() = _binding!!
     private var noteRecyclerAdapter: NoteRecyclerAdapter? = null
     private var courseRecyclerAdapter: CourseRecyclerAdapter? = null
     private lateinit var adapterNotes: ArrayAdapter<NoteInfo>
+    private lateinit var dbOpenHelper: NoteKeeperOpenHelper
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,6 +41,17 @@ class HomeFragment : Fragment() {
         val root: View = binding.root
         initializeDisplayContent()
         return root
+    }
+
+
+    override fun onAttach(activity: Activity) {
+        dbOpenHelper = NoteKeeperOpenHelper(activity.baseContext)
+        super.onAttach(activity)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        dbOpenHelper.close()
     }
 
     override fun onResume() {
@@ -54,6 +69,8 @@ class HomeFragment : Fragment() {
 
 
     private fun initializeDisplayContent() {
+        DataManager.instance?.loadFromDatabase(dbOpenHelper)
+        noteListSize = DataManager.instance?.notes?.size!!
         displayNotes()
     }
 
